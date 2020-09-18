87e28df8f1f58effe00b91ec5554debddde258e3
Mongoose556 <seanwilson1981@gmail.com>
2020-09-18 23:30:38 +0100

    refactored file reading, male female pic method

--- person.py
+++ person.py
@@ -6,58 +6,87 @@ logging.basicConfig(filename="person.log", level=logging.INFO, format='%(levelna
 
 #https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
 
-# TODO: make attributes assignable, e.g. .Age = 25, Name = dave etc 
-# TODO: read names etc from a file
-# TODO: age missing from output
-
 class Person:
-	""" Creates an instance of the Person class, 
-	Reads the data files """
+	""" Creates an instance of the Person class, reads the data files """
 
 	def __init__(self):
-
+		# TODO: Is there a better way of doing this? Try / Except?
 		#read name files and log errors
 		from pathlib import Path
 
-		if not Path("data/female_names.json").is_file():
-			logging.info("Could not find file! data/female_names.json")
-			raise ValueError(f"Could not find file! data/female_names.json")
+		# Female
+		filename = "data/female_names.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
 
-		# Male
-		if not Path("data/male_names.json").is_file():
-			logging.info("Could not find file! data/male_names.json")
-			raise ValueError(f"Could not find file! data/male_names.json")
-			
-		# Surnames
-		if not Path("data/surnames.json").is_file():
-			logging.info("Could not find file! data/surnames.json")
-			raise ValueError(f"Could not find file! data/surnames.json")
-			
-		# job's
-		if not Path("data/jobs.json").is_file():
-			logging.info("Could not find file! data/jobs.json")
-			raise ValueError(f"Could not find file! data/jobs.json")
-			
-		# assign data
-		# male names
-		with open("data/male_names.json", 'r') as json_file:
-			data = json.load(json_file)
-			self.male_first_name = data['male_forenames']
 
-		# female names
 		with open("data/female_names.json", 'r') as json_file:
 			data = json.load(json_file)
 			self.female_first_name = data['female_forenames']
+		###
 
-		# surnames
-		with open("data/surnames.json", 'r') as json_file:
+		# Male
+		filename = "data/male_names.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
+
+		with open(filename, 'r') as json_file:
+			data = json.load(json_file)
+			self.male_first_name = data['male_forenames']
+
+		# Surnames
+		filename = "data/surnames.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
+
+		with open(filename, 'r') as json_file:
 			data = json.load(json_file)
 			self.surname = data['surnames']
+		
+		# jobs
+		filename = "data/jobs.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
 
-		# job's
+
 		with open("data/jobs.json", 'r') as json_file:
 			data = json.load(json_file)
 			self.job = data['occupations']
+
+		# Bio
+		filename = "data/bio.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
+
+		with open(filename, 'r') as json_file:
+			data = json.load(json_file)
+			self.bio = data['info']
+
+		# female pictures
+		filename = "data/female_pics.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
+
+		with open(filename, 'r') as json_file:
+			data = json.load(json_file)
+			self.female_profile_pic = data['pictures']
+
+		# male pictures
+		filename = "data/male_pics.json"
+		if not Path(filename).is_file():
+			logging.info(f"Could not find file! {filename}")
+			raise ValueError(f"Could not find file! {filename}")
+
+		with open(filename, 'r') as json_file:
+			data = json.load(json_file)
+			self.male_profile_pic = data['pictures']
+
 
 ### ### Main Methods ### ### 
 
@@ -80,7 +109,7 @@ class Person:
 
 
 	def gender(self, gender=None):
-		""" Assigns or returns a gender (str) """
+		""" Assigns or returns a gender (str), if None provided then random choice Male/Female """
 		if gender == None:
 			self.gender = random.choice(["male", "female"])
 		else:
@@ -99,46 +128,46 @@ class Person:
 
 
 	def job(self, job=""):
+		""" Assigns a job or returns a list of jobs """
 		self.job = job
 		return self.job
 
-
 	def create_random(self):
 		""" Returns a tuple: Gender, Firstname, Lastname, Age, job, Picture """
-		g = self.gender()
+		gender = self.gender()
 
-		if g == "male":
+		if gender == "male":
 			firstname = random.choice(self.male_first_name)
+			pic = random.choice(self.male_profile_pic)
 		else:
 			firstname = random.choice(self.female_first_name)
+			pic = random.choice(self.male_profile_pic)
 
-		a = self.age()
+		age = self.age()
+		#birth_year = self.year_of_birth()
 		surname = random.choice(self.surname)
 		job = random.choice(self.job)
-		pic = self.profile_pic()
-		# return as dict?
 
 		x = ()
 
-		x = (g, firstname, surname, a, job, pic)
+		x = (gender, firstname, surname, age, birth_year, job, pic)
 
 		return x
 
+	def female_profile_pic(self):
+		""" Returns a list of pictures """
+		return self.female_profile_pic
 
-	def profile_pic(self):
-		""" Returns a random picture """
-		if self.gender == "male":
+	def male_profile_pic(self):
+		""" Returns a list of pictures """
+		return self.male_profile_pic
+	
+	'''
+	def year_of_birth(self):
+		import datetime
+		today = datetime()
+		year = today.datetime.today
+		yob = year - self.age
+		return yob
 
-			male_picture = (
-				"male_pic1.jpg",
-				"male_pic2.jpg",
-				"male_pic3.jpg"
-				)
-			return random.choice(male_picture)
-		else:
-			female_picture = (
-				"female_pic1.jpg",
-				"female_pic2.jpg",
-				"female_pic3.jpg"
-			)
-		return random.choice(female_picture)
+	'''
